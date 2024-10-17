2d3ee7f9fee8bc06c25ba622d552ef9d110eb99d
Rasemouse <rasmuswenngren@gmail.com>
2024-10-17 10:21:37 +0200

    First commit

---
+++ Count Occurences/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Security;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Count_Occurences;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CountOccurrences("Hello", 'l');
+    }
+
+    public static int CountOccurrences(string str, char c)
+    {
+        string lowerStr = str.ToLower();
+        string lowerChar = c.ToString().ToLower();
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == c)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
+
